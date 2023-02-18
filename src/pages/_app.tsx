1fcf973262d54cfe0ede8d7ef65d4f72561cc551
bubuding0809 <bubuding0809@gmail.com>
2023-02-18 17:24:04 +0800

    Fixing app type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ import "animate.css";
 import { ReactElement, ReactNode } from "react";
 import { NextPage } from "next";
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
 
