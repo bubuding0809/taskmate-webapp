ebc69daeec60b7b87a9a4089c41131bc4b61de94
bubuding0809 <bubuding0809@gmail.com>
2023-02-18 21:11:27 +0800

    Changed to client side session authentication since getServerSideProps does not want to work with the board

--- src/pages/board/[bid].tsx
+++ src/pages/board/[bid].tsx
@@ -6,30 +6,24 @@ import BoardView from "../../components/Board/BoardView";
 import { getServerSession, Session } from "next-auth";
 import { authOptions } from "../../server/auth";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
-interface UserBoardPageProps {
-  session: string;
-  bid: string;
-}
-const UserBoardPage: NextPageWithLayout<UserBoardPageProps> = ({
-  session,
-  bid,
-}) => {
-  const [showBoard, setShowBoard] = useState(false);
-
-  // Add a mandatory delay below rendering board to prevent Vercel Serverless Function from timing out
-  useEffect(() => {
-    const timeOut = setTimeout(() => {
-      setShowBoard(true);
-    }, 500);
-    return () => clearTimeout(timeOut);
-  }, []);
+const UserBoardPage: NextPageWithLayout = () => {
+  const { data: sessionData, status } = useSession();
+  const router = useRouter();
+  const { bid } = router.query as { bid: string };
 
   return (
     <>
-      {showBoard ? (
-        <BoardView bid={bid} />
-      ) : (
+      {sessionData && status === "authenticated" && <BoardView bid={bid} />}
+      {status === "unauthenticated" && (
+        <div className="flex h-screen flex-col items-center justify-center">
+          <h1 className="text-w<BoardView bid={bid} />hite text-4xl font-bold">
+            You must be signed in to view this page.
+          </h1>
+        </div>
+      )}
+      {status === "loading" && (
         <div className="flex h-screen flex-col items-center justify-center">
           <h1 className="text-w<BoardView bid={bid} />hite text-4xl font-bold">
             Loading...
@@ -43,38 +37,4 @@ export default UserBoardPage;
 
 UserBoardPage.getLayout = (page: ReactElement) => {
   return <AppLayout title="Test Board">{page}</AppLayout>;
-};
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  // const session = await getServerSession(context.req, context.res, authOptions);
-  const bid = context.params?.bid as string;
-
-  if (typeof bid !== "string" || bid.length === 0) {
-    return {
-      notFound: true,
-    };
-  }
-
-  // use timeout to simulate slow network
-  const session = await new Promise((resolve) => {
-    setTimeout(() => {
-      return resolve("Session");
-    }, 1);
-  });
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/api/auth/signin",
-        permanent: false,
-      },
-    };
-  }
-
-  return {
-    props: {
-      bid,
-      session,
-    },
-  };
 };
