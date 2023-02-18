c73a4a27273c8aabafb27aec2aeb055e102b78af
bubuding0809 <bubuding0809@gmail.com>
2023-02-18 18:09:48 +0800

    Changed demo route from /board to /demo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,7 @@ const Home: NextPage = () => {
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="/board"
+              href="/demo"
             >
               <h3 className="text-2xl font-bold">Live Demo→</h3>
               <div className="text-lg">
