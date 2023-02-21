3bc483e4aa893a76e81013be47c0470101cbf12c
bubuding0809 <bubuding0809@gmail.com>
2023-02-21 15:38:30 +0800

    Added two new routers, folder and board

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
 import { createTRPCRouter } from "./trpc";
 import { exampleRouter } from "./routers/example";
+import { folderRouter } from "./routers/folder";
+import { boardRouter } from "./routers/board";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +10,8 @@ import { exampleRouter } from "./routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  folder: folderRouter,
+  board: boardRouter,
 });
 
 // export type definition of API
