f1debfa0bb5e0762c91c5ad457109c8a1063fa7b
bubuding0809 <bubuding0809@gmail.com>
2023-03-05 10:19:16 +0800

    Added doc strings for helpers

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,9 +1,20 @@
 // Used to handle conditional class names
 // Usage: <div className={classNames("text-red-500", "text-lg", "font-bold", "text-center")}></div>
-export function classNames(...classes: (string | boolean)[]) {
+/**
+  @param {string[]} classes - An array of class names to conditionally add to the element
+  @returns {string} The class names to add to the element
+**/
+export function classNames(...classes: (string | boolean)[]): string {
   return classes.filter(Boolean).join(" ");
 }
 
+// This helper function is used to format a date object into a string
+// The function will return a string representing the date in the format "Today, Time" if the date is today
+// Else it will return a string representing the date in the format "DayOfWeek, Day Month, Time"
+/**
+  @param {Date} date - The date object to format
+  @returns {string} The formatted date string
+**/
 export function formatDate(date: Date): string {
   // Extract and format time of the date object
   const time = date
@@ -28,4 +39,17 @@ export function formatDate(date: Date): string {
   const day = date.getDate();
 
   return `${dayOfWeekName}, ${day} ${monthName}, ${time}`;
+}
+
+/**
+  @param {string[]} characters - An array of characters to trim from the string
+  @param {string} str - The string to trim
+  @returns {string} The trimmed string
+**/
+export function trimChar(characters: string[], str: string): string {
+  const regex = new RegExp(
+    `^[${characters.join("")}]+|[${characters.join("")}]+$`,
+    "g"
+  );
+  return str.replace(regex, "");
 }
