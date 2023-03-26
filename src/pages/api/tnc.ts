9cd14c74f68d47c73b31b9d2a7f3a0b4d2ecc1d9
bubuding0809 <bubuding0809@gmail.com>
2023-03-27 02:56:47 +0800

    fix tnc reading

--- src/pages/api/tnc.ts
+++ src/pages/api/tnc.ts
@@ -2,6 +2,7 @@
 import { promises as fsPromises } from "fs";
 import * as path from "path";
 import { NextApiRequest, NextApiResponse } from "next";
+import { tnc } from "../../server/info/tnc.json";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "GET") {
@@ -9,13 +10,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   try {
-    const fileContents = await fsPromises.readFile(
-      path.join("src/server/info", "tnc.txt"),
-      {
-        encoding: "utf8",
-      }
-    );
-    res.status(200).json({ message: fileContents });
+    res.status(200).json({ message: tnc });
   } catch (err) {
     console.log("error is: ", err);
   }
