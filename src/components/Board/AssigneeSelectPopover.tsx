b4c3413bc3ef0020dc5dd3b13d0891d1ee84478e
bubuding0809 <bubuding0809@gmail.com>
2023-05-01 13:51:36 +0800

    Extracted NewTaskFormType to utils

--- src/components/Board/AssigneeSelectPopover.tsx
+++ src/components/Board/AssigneeSelectPopover.tsx
@@ -11,7 +11,7 @@ import useAddAssignees from "@/utils/mutations/task/useAddAssignees";
 import type { User } from "@prisma/client";
 import type { Optional } from "@/utils/types";
 import type { RouterOutputs } from "@/utils/api";
-import type { JSONContent } from "@tiptap/react";
+import type { NewTaskFormType } from "@/utils/types";
 
 type ExtractPanel<T> = T extends { Panel: infer U } ? U : never;
 type Panel = ExtractPanel<RouterOutputs["board"]["getBoardById"]>[number];
@@ -19,24 +19,8 @@ type Task = Optional<Panel["Task"][number], "subtasks">;
 
 interface UserSearchPopoverProps {
   setPopOverOpen?: Dispatch<SetStateAction<boolean>>;
-  newTaskForm?: {
-    task_title: string;
-    task_description: JSONContent;
-    task_start_dt: string;
-    task_end_dt: string;
-    task_due_dt: string;
-    task_assignedUsers: User[];
-  };
-  setNewTaskForm?: React.Dispatch<
-    React.SetStateAction<{
-      task_title: string;
-      task_description: JSONContent;
-      task_start_dt: string;
-      task_end_dt: string;
-      task_due_dt: string;
-      task_assignedUsers: User[];
-    }>
-  >;
+  newTaskForm?: NewTaskFormType;
+  setNewTaskForm?: React.Dispatch<React.SetStateAction<NewTaskFormType>>;
   bid: string;
   task?: Task;
   innerClassName?: string;
