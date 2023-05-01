9b7bdaf76ef1a2f428580a9c8e1d6bca7744990d
bubuding0809 <bubuding0809@gmail.com>
2023-05-01 13:50:52 +0800

    Refactored client-side types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,56 +1,7 @@
-/* DONE BY: Ding RuoQian 2100971 */
+// Type definitions for the client-side application
 
-import { Moment } from "moment";
-
-// Type definition for a new entry to be added to the todo list
-// A new entry is a collection of content that is to be turned into a task
-export type EntryType = {
-  readonly todoMessage: string;
-  readonly todoDateTime: Moment | null;
-  readonly todoDescription: string;
-};
-
-// Type definition for a task
-// A task is a collection of content
-export type TaskType = {
-  id: string;
-  parent: string | null;
-  title: string;
-  date: string;
-  time: string;
-  description: string;
-  subtasks: string[];
-  isCompleted: boolean;
-};
-
-// Type definition for a list of tasks
-// A list of tasks is a collection of tasks
-export type TasksType = {
-  [key: string]: TaskType;
-};
-
-// Type definition for a panel
-// A panel is a collection of tasks
-export type PanelType = {
-  id: string;
-  title: string;
-  active: string[];
-  completed: string[];
-};
-
-// Type definition for a list of panels
-// A list of panels is a collection of panels
-export type PanelsType = {
-  [key: string]: PanelType;
-};
-
-// Type definition for the board
-// A board is a collection of panels
-export type BoardType = {
-  todoTasks: TasksType;
-  panels: PanelsType;
-  panelOrder: string[];
-};
+import { User } from "@prisma/client";
+import { JSONContent } from "@tiptap/react";
 
 export type EmojiType = {
   aliases: string[];
@@ -60,6 +11,15 @@ export type EmojiType = {
   native: string;
   shortcodes: string;
   unified: string;
+};
+
+export type NewTaskFormType = {
+  task_title: string;
+  task_description: JSONContent | null;
+  task_start_dt: string;
+  task_end_dt: string;
+  task_due_dt: string;
+  task_assignedUsers: User[];
 };
 
 // Optional utility type
